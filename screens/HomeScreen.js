0f83a65019647a649fdf71ae15fe166b6d130d75
unknown <fatih969692@gmail.com>
2021-08-18 01:15:28 +0300

    first-commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { View, SafeAreaView, Image } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
+import NavFavourites from '../components/NavFavourites';
+
+const HomeScreen = () => {
+    const dispatch = useDispatch();
+
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: 'contain'
+                    }}
+                    source={{
+                        uri: "https://links.papareact.com/gzs"
+                    }}
+                />
+                <GooglePlacesAutocomplete
+                    placeholder="Where From?"
+                    returnKeyType={"search"}
+                    styles={{
+                        container: {
+                            flex: 0
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        }
+                    }}
+                    minLength={2}
+                    enablePoweredByContainer={false}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en',
+                    }}
+
+                    onFail={error => console.error(error)}
+                    onPress={(data, details = null) => {
+                        dispatch(
+                            setOrigin({
+                                location: details.geometry.location,
+                                description: data.description
+                            }));
+                        dispatch(setDestination(null))
+                    }}
+                    fetchDetails={true}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
+
+                />
+                <NavOptions />
+                <NavFavourites />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
